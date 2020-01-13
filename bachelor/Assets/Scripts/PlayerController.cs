b06ef757a86a67fc22fb50f884dcba29e7624b46
Sivikiwi93 <sivikiwi93@hotmail.com>
2020-01-13 15:26:01 +0100

    Adds game to repo

---
+++ bachelor/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Movement variables
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    //Player Animation
+    public Animator animator;
+
+    void Update()
+    {
+        //Handles Input for movement
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //Handles animation
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    void FixedUpdate()
+    {
+        //Handles movement
+        rb.MovePosition(rb.position + (movement * moveSpeed * Time.fixedDeltaTime));
+    }
+}
