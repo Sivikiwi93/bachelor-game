b07afbc664092afebb3bff176e80acbd1e9fe724
Sivikiwi93 <sivikiwi93@hotmail.com>
2020-01-22 22:00:22 +0100

    Paint map, adds laser gate

---
+++ bachelor/Assets/Scripts/LaserGateAudio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserGateAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Play start audio of something smashing into a laser
+        Debug.Log("Player touched laser");
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //Play audio loop for laser touching something
+        Debug.Log("Player keeps touching the laser");
+    }
+}
