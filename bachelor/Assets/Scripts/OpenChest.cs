f83a59153498697541564026878d8ff47b5b951e
Sivikiwi93 <sivikiwi93@hotmail.com>
2020-02-12 14:49:11 +0100

    Chest base implemented

---
+++ bachelor/Assets/Scripts/OpenChest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenChest : MonoBehaviour
+{
+
+    public Sprite openChest;
+    SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       sr = GetComponent<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        sr.sprite = openChest;
+    }
+}
