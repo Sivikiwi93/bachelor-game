11125041165e29db70f3c43a3ba42a54e11990c3
Sivikiwi93 <sivikiwi93@hotmail.com>
2020-02-12 20:02:30 +0100

    Piano implement

--- bachelor/Assets/Scripts/PianoKeyPlay.cs
+++ bachelor/Assets/Scripts/PianoKeyPlay.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PianoKeyPlay : MonoBehaviour
 {
+
+    public AudioClip c;
+    AudioSource audioSource;
+    public float semitoneOffset;
+
+    public KeyCode key;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(key))
+        {
+            PlayKey();
+        }
+    }
+
+    public void PlayKey()
+    {
+        audioSource.pitch = Mathf.Pow(2f, semitoneOffset/12.0f);
+        audioSource.PlayOneShot(c);
     }
 }
