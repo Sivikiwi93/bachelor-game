33997b16a10069fff9290523b3c6289201d44f10
Sivikiwi93 <sivikiwi93@hotmail.com>
2020-02-19 16:38:53 +0100

    CameraRework

---
+++ bachelor/Assets/Scripts/CameraSwap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwap : MonoBehaviour
+{
+
+    public GameObject virtualCam;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            virtualCam.SetActive(true);
+            //Debug.Log("camera on");
+        }
+
+        //Debug.Log("TRIGGERED");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            virtualCam.SetActive(false);
+        }
+    }
+}
