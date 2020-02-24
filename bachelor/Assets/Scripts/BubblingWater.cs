8b7694ec021483356ecbecdf49c1ba0b1370e721
Sivikiwi93 <sivikiwi93@hotmail.com>
2020-02-24 16:32:47 +0100

    Implemented water script room 6

---
+++ bachelor/Assets/Scripts/BubblingWater.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BubblingWater : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    public TileBase water;
+    public TileBase sand;
+
+    public Tilemap tilemap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            tilemap.SwapTile(water, sand);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        audioSource.Play();
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        audioSource.Stop();
+    }
+}
