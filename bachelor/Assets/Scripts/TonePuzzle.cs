c45f62a177b287d50e538d62daefc97182c32836
Sivikiwi93 <sivikiwi93@hotmail.com>
2020-02-24 15:36:23 +0100

    Puzzle 2 implemented

---
+++ bachelor/Assets/Scripts/TonePuzzle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class TonePuzzle : MonoBehaviour
+{
+
+    public float[] bushOrder = new float[4];
+    private float[] setOrder = {1.0f, 1.1f, 1.25f, 1.3f};
+    private int counter;
+
+    public GameObject laser;
+    public LaserGateAudio lga;
+
+    private void Start()
+    {
+        counter = 0;
+
+    }
+
+
+    public void AddAndCheckList(float note)
+    {
+
+        bushOrder[counter] = note;
+        counter++;
+
+        if (counter == 4)
+        {
+            if(Enumerable.SequenceEqual(bushOrder, setOrder))
+            {
+                lga.TurningOff();
+                laser.SetActive(false);
+
+                Debug.Log("Shit Works with the combobroooo!!!!!");
+                counter = 0;
+                Array.Clear(bushOrder, 0, 4);
+            }
+            else
+            {
+                //Spill lyd som sier feil
+                Array.Clear(bushOrder, 0, 4);
+                counter = 0;
+            }
+        }
+    }
+}
---
+++ bachelor/Assets/Scripts/TriggerBushTone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBushTone : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float bushNR;
+    public TonePuzzle tonePz;
+
+    private void Update()
+    {
+        //if () { }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.Q))
+        {
+            audioSource.Play();
+            bushNR = audioSource.pitch;
+            tonePz.AddAndCheckList(bushNR);
+        }
+    }
+
+}
