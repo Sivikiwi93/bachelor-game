7a4e3ad9dcf8e7cc601bbb5c0aa4f396fc7e1875
Sivikiwi93 <sivikiwi93@hotmail.com>
2020-02-26 17:49:50 +0100

    Random music picking & new music track

---
+++ bachelor/Assets/Scripts/MusicRandomPick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicRandomPick : MonoBehaviour
+{
+
+    public AudioClip[] music;
+    public AudioSource audioSource;
+
+    private int musicSelection;
+
+    void Start()
+    {
+        PlayMusic();
+    }
+
+    public void PlayMusic()
+    {
+        musicSelection = Random.Range(0, music.Length);
+        audioSource.clip = music[musicSelection];
+        audioSource.Play();
+    }
+}
