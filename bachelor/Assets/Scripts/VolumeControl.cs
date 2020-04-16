2128ddc6a30c4c351fdb560fa0ba229a7bcd0be0
sivikiwi93 <sivikiwi93@hotmail.com>
2020-04-16 13:32:35 +0200

    Volume control added to menus

---
+++ bachelor/Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeControl : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+
+    public void SetMusicLevel(float sliderValue)
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetSFXLevel(float sliderValue)
+    {
+        mixer.SetFloat("SFXVol", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetMasterLevel(float sliderValue)
+    {
+        mixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
+    }
+}
